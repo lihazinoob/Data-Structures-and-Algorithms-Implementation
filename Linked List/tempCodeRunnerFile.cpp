47b6271bfe8d298d03e308840ef1110eae072a11
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-07-28 10:44:40 +0600

    Creating Node for Linked List is Successfully done

--- Linked List/tempCodeRunnerFile.cpp
+++ Linked List/tempCodeRunnerFile.cpp
@@ -8,19 +8,13 @@ class Node{
   int data;
   Node* nextpointer;
 
-  // Creating a Constructor to create a Node with manual data
-  
-  Node(int  data)
-  {
-    this->data = data;
-    this->nextpointer = NULL;
-  }
+
 
 };
 
 int main ()
 {
-  Node* node1 = new Node(100); // Remember, new keyword returns a pointer to the allocated object in C++
+  Node* node1 = new Node(); // Remember, new keyword returns a pointer to the allocated object in C++
   cout<<node1->data<<endl;
   cout<<node1->nextpointer;
 }
